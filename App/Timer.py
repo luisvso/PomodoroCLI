669691261b9a20bb86b52c1e98ee721d3aa2bfdd
LuisVvS <luluisv7@gmail.com>
2025-02-17 21:45:26 -0300

    added the downtime but with a little problems

--- App/Timer.py
+++ App/Timer.py
@@ -1,2 +1,25 @@
+import time
+from rich.progress import Progress
+
 def Timer():
-    print("Timer working")
+    sessions = int(input("Ho many sessions you are going to take?"))
+    focus = int(input("What is your focus time?"))
+    breaks = int(input("What is your break time?"))
+
+    #The countdown function to run the time
+    countdown(focus)
+
+
+def countdown(f):
+    with Progress() as progress:
+        seconds = f * 60
+        task = progress.add_task("Focusing....", total=seconds)
+        for i in range(seconds):
+            time.sleep(1)
+            seconds-=1
+            progress.update(task, advance=1)
+        print("⏰ Time's up!")
+
+
+def sessions_loop(s):
+    ...
