dae1f1af12d193dbb6a26004e9224997c3c4523c
LuisVvS <luluisv7@gmail.com>
2025-02-21 23:58:02 -0300

    added the break loop, and fixed the timer

--- App/Timer.py
+++ App/Timer.py
@@ -8,18 +8,44 @@ def Timer():
 
     #The countdown function to run the time
     countdown(focus)
+    breaker(breaks)
 
 
 def countdown(f):
-    with Progress() as progress:
-        seconds = f * 60
-        task = progress.add_task("Focusing....", total=seconds)
-        for i in range(seconds):
-            time.sleep(1)
-            seconds-=1
-            progress.update(task, advance=1)
-        print("⏰ Time's up!")
+    respond = "nao"
+    while respond == "nao":
+        respond = input("You want to start the focusing timer?")
 
+    if(respond == "sim"):
+        with Progress() as progress:
+            seconds = f * 60
+            task = progress.add_task("Focusing....", total=seconds)
+            for i in range(seconds):
+                time.sleep(0.9)
+                seconds-=1
+                progress.update(task, advance=1)
+            print("⏰ Time's up!") 
+
+
+def breaker(breaker):
+    respond = "nao"
+    while respond == "nao":
+        respond = input("Start Break?")
+
+
+    if respond == "sim":
+        seconds = breaker * 60
+        if(respond == "sim"):
+            with Progress() as progress:
+                task = progress.add_task("Resting....", total=seconds)
+                for i in range(seconds):
+                    time.sleep(0.9)
+                    seconds-=1
+                    progress.update(task, advance=1) 
+                print("The break if over") 
+
+
+    
 
 def sessions_loop(s):
     ...
